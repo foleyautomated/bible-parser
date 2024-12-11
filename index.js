318ddfa0e63e0dee406f2e724a9ba90046dcf3d1
David Foley <foleyautomated@gmail.com>
2024-12-11 15:53:18 -0500

    added generes

--- index.js
+++ index.js
@@ -2,40 +2,67 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log("Hello from my TypeScript CLI!");
+function removeDuplicateObjects(jsonArray, key) {
+    var uniqueObjects = new Map();
+    return jsonArray.filter(function (obj) {
+        var keyValue = obj[key];
+        if (!uniqueObjects.has(keyValue)) {
+            uniqueObjects.set(keyValue, true);
+            return true;
+        }
+        return false;
+    });
+}
 function parseBibleText(bibleText) {
     var books = [];
     var lines = bibleText.split('\n');
-    var currentBook = null;
-    var currentChapter = 0;
-    lines.forEach(function (line) {
-        var _a = line.split('\t'), reference = _a[0], verseText = _a[1];
-        var _b = reference.split(' '), bookName = _b[0], chapterVerse = _b[1];
-        var _c = chapterVerse.split(':').map(Number), chapter = _c[0], verse = _c[1];
-        if (!currentBook || currentBook.name !== bookName) {
-            if (currentBook) {
-                books.push(currentBook);
-            }
-            currentBook = {
-                name: bookName,
-                abbreviation: bookName.slice(0, 3).toUpperCase(),
-                chapters: {}
-            };
-            currentChapter = chapter;
+    var prevBookName = "Genesis";
+    var prevChapterNumber;
+    var currTestament = "OT";
+    lines.forEach(function (line, index) {
+        var _a;
+        var chapter;
+        var verse;
+        var bookName;
+        ;
+        var content;
+        var lineSegments = line.split(/\s/);
+        //Get Book Name (Handle '1 Samuel', '3 John', etc)
+        if (line.startsWith('1') || line.startsWith('2') || line.startsWith('3')) {
+            bookName = "".concat(lineSegments[0], " ").concat(lineSegments[1]);
+            lineSegments = lineSegments.slice(2);
         }
-        if (currentChapter !== chapter) {
-            currentChapter = chapter;
+        else {
+            bookName = lineSegments[0].trim();
+            lineSegments = lineSegments.slice(1);
         }
-        if (!currentBook.chapters[currentChapter]) {
-            currentBook.chapters[currentChapter] = 0;
+        //Get Chapter and Verse Numbers
+        _a = line.match(/\d+:\d+/)[0].split(":").map(function (val) { return parseInt(val.trim()); }), chapter = _a[0], verse = _a[1];
+        if (bookName == "Matthew") {
+            currTestament = "NT";
         }
-        currentBook.chapters[currentChapter]++;
+        //console.log(`${bookName} ${chapter}>>${verse} ${content}`)
+        //console.log(currentBibleBook.chapters + "----" + chapter);
+        if (prevChapterNumber < chapter || prevChapterNumber == undefined) {
+            prevChapterNumber = chapter;
+        }
+        else if (chapter < prevChapterNumber && prevBookName != bookName || index == lines.length - 1) {
+            books.push({
+                name: prevBookName,
+                testament: currTestament,
+                abbreviation: abbreviations[prevBookName],
+                chapters: prevChapterNumber,
+                genres: genres[prevBookName]
+            });
+            prevBookName = bookName;
+            prevChapterNumber = 0;
+        }
     });
-    if (currentBook) {
-        books.push(currentBook);
-    }
     return books;
 }
 var fs = require("fs");
 var bibleText = fs.readFileSync('./data/asv.txt', 'utf8');
+var abbreviations = JSON.parse(fs.readFileSync('./data/abbreviations.json', 'utf8'));
+var genres = JSON.parse(fs.readFileSync('./data/genres.json', 'utf8'));
 var biblejson = parseBibleText(bibleText);
-fs.writeFileSync('bible.json', JSON.stringify(biblejson, null, 2), 'utf8');
+fs.writeFileSync('bibleMetadata.json', JSON.stringify(biblejson, null, 2), 'utf8');
--- index.ts
+++ index.ts
@@ -1,49 +1,82 @@
 #!/usr/bin/env node
-console.log("Hello from my TypeScript CLI!");
+console.log("Extracting Biblical Metadata...");
 
 interface BibleBook {
     name: string;
+    testament: string;
     abbreviation: string;
-    chapters: { [chapter: number]: number };
+    chapters: number;
+    genres: string[];
 }
+
+interface BibleChapter {
+    number: number;
+    verses: { [verse: number]: string };
+}
+
+function removeDuplicateObjects(jsonArray: any[], key: string): any[] {
+    const uniqueObjects = new Map();
+  
+    return jsonArray.filter(obj => {
+      const keyValue = obj[key];
+      if (!uniqueObjects.has(keyValue)) {
+        uniqueObjects.set(keyValue, true);
+        return true;
+      }
+      return false;
+    });
+  }
 
 function parseBibleText(bibleText: string): BibleBook[] {
     const books: BibleBook[] = [];
     const lines = bibleText.split('\n');
-    let currentBook: BibleBook | null = null;
-    let currentChapter = 0;
 
-    lines.forEach(line => {
-        const [reference, verseText] = line.split('\t');
-        const [bookName, chapterVerse] = reference.split(' ');
-        const [chapter, verse] = chapterVerse.split(':').map(Number);
+    let prevBookName = "Genesis"
+    let prevChapterNumber: number;
+    let currTestament = "OT"
 
-        if (!currentBook || currentBook.name !== bookName) {
-            if (currentBook) {
-                books.push(currentBook);
-            }
-            currentBook = {
-                name: bookName,
-                abbreviation: bookName.slice(0, 3).toUpperCase(),
-                chapters: {}
-            };
-            currentChapter = chapter;
+    lines.forEach((line, index) => {
+
+        let chapter: number;
+        let verse: number;
+        let bookName: string;;
+        let content: string;
+
+        let lineSegments = line.split(/\s/);
+
+        //Get Book Name (Handle '1 Samuel', '3 John', etc)
+        if (line.startsWith('1') || line.startsWith('2') || line.startsWith('3')) {
+            bookName = `${lineSegments[0]} ${lineSegments[1]}`
+            lineSegments = lineSegments.slice(2);
+        } else {
+            bookName = lineSegments[0].trim();
+            lineSegments = lineSegments.slice(1);
+        }
+        
+        //Get Chapter and Verse Numbers
+        [chapter, verse] = line.match(/\d+:\d+/)![0].split(":").map(val => parseInt(val.trim()));
+
+        if(bookName == "Matthew") {
+            currTestament = "NT"
         }
 
-        if (currentChapter !== chapter) {
-            currentChapter = chapter;
-        }
+        //console.log(`${bookName} ${chapter}>>${verse} ${content}`)
 
-        if (!currentBook.chapters[currentChapter]) {
-            currentBook.chapters[currentChapter] = 0;
+        //console.log(currentBibleBook.chapters + "----" + chapter);
+        if(prevChapterNumber < chapter || prevChapterNumber == undefined) {
+            prevChapterNumber = chapter;
+        } else if( chapter < prevChapterNumber && prevBookName != bookName || index == lines.length - 1) {
+            books.push({
+                name: prevBookName,
+                testament: currTestament,
+                abbreviation: abbreviations[prevBookName],
+                chapters: prevChapterNumber,
+                genres: genres[prevBookName]
+            }); 
+            prevBookName = bookName;
+            prevChapterNumber = 0;
         }
-
-        currentBook.chapters[currentChapter]++;
     });
-
-    if (currentBook) {
-        books.push(currentBook);
-    }
 
     return books;
 }
@@ -51,7 +84,9 @@ function parseBibleText(bibleText: string): BibleBook[] {
 import * as fs from 'fs';
 
 const bibleText = fs.readFileSync('./data/asv.txt', 'utf8');
-
+const abbreviations = JSON.parse(fs.readFileSync('./data/abbreviations.json', 'utf8'));
+const genres = JSON.parse(fs.readFileSync('./data/genres.json', 'utf8'));
 const biblejson = parseBibleText(bibleText);
 
-fs.writeFileSync('bible.json', JSON.stringify(biblejson, null, 2), 'utf8');
+fs.writeFileSync('bibleMetadata.json', JSON.stringify(biblejson, null, 2), 'utf8');
+
